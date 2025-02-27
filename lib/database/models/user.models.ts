146e3793f6baa3f7eab646ff368cebccade086be
frankamale <frankamale5@gmail.com>
2025-02-27 22:36:17 +0300

    transactions schema

--- lib/database/models/user.models.ts
+++ lib/database/models/user.models.ts
@@ -1,28 +1,15 @@
 import { model, models, Schema } from "mongoose";
-
-
-export interface User extends Document{
-    clerkId: string,
-    email:string,
-    username: string,
-    photo: string,
-    firstName:string,
-    lastName:string,
-    planId:string,
-    creditBalance:string
-}
-
 const UserSchema = new Schema({
-    clerkId: {type: String, required: true},
-    email: {type: String, required: true},
-    username: {type: String, required: true},
-    photo: {type: String},
+    clerkId: {type: String, required: true, unique:true},
+    email: {type: String, required: true, unique:true},
+    username: {type: String, required: true, unique:true},
+    photo: {type: String, required:true},
     firstName:{type: String},
     lastName:{type: String},
-    planId:{type: String},
-    creditBalance:{type :Number}
+    planId:{type: Number, default:1},
+    creditBalance:{type :Number, default:10}
 })
 
 const User = models?.User || model("User", UserSchema)
 
-export default UserSchema;
+export default User;
