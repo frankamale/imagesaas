1a028b82529117647100e430df38c633d91f42d7
frankamale <frankamale5@gmail.com>
2025-03-12 16:28:20 +0300

    trsnsformation pages

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTranformationTypePage = () => {
-  return (
-    <div>AddTranformationTypePage</div>
-  )
-}
+const AddTranformationTypePage = async ({  params: { type }}:
+   SearchParamProps) => {
+    const { userId } = await auth();
+    const transformation = transformationTypes[type];
 
-export default AddTranformationTypePage
+    if (!userId) redirect('/sign-in');
+    const user = await getUserById(userId);
+  
+  return(
+    <>
+    <Header 
+    title={transformation.title}
+    subtitle={transformation.subTitle}
+    />
+
+    <TransformationForm
+      action="Add"
+      userId={user._id}
+      type = {transformation.type as TransformationTypeKey}
+      creditBalance={ user.creditBalance}
+    />
+    </>
+  );
+};
+
+export default AddTranformationTypePage;
